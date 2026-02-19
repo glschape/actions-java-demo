7fdc8faa5fb10a01ec8341d50f062c423f10b5d9
glschape <glschape@mtu.edu>
2026-02-18 20:10:12 -0500

    Correcting for yml run Hello.java
    changing file error

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class test {
-  public static void main(String[] args) {
-      System.out.println("Hello");
-  }
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello from GitHub Actions");
+    }
 }
